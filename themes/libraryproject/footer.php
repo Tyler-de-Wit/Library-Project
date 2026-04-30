64b79a4a8c141a70b481658ebb2b784ac85e71cb
Tyler <87571592+Tila05@users.noreply.github.com>
2026-04-30 17:18:50 +0800

    Change contacts in footer to directly link to send email, call and goole maps respectfully

--- themes/libraryproject/footer.php
+++ themes/libraryproject/footer.php
@@ -16,9 +16,9 @@
         <section class="col-12 col-lg-4 mt-3">
             <h2>Contacts</h2>
             <ul>
-                <li><i class="bi bi-envelope me-2"></i>Email: library@tafe.wa.edu.au</li>
-                <li><i class="bi bi-telephone me-2"></i>Phone: 1300 300 822</li>
-                <li><i class="bi bi-geo-alt me-2"></i>Location: 25 Aberdeen St, Perth WA 6000</li>
+                <li><i class="bi bi-envelope me-2"></i>Email: <a href="mailto:library@tafe.wa.edu.au">library@tafe.wa.edu.au</a></li>
+                <li><i class="bi bi-telephone me-2"></i>Phone: <a href="tel:1300 300 822">1300 300 822</a></li>
+                <li><i class="bi bi-geo-alt me-2"></i>Location: <a href="https://www.google.com/maps/place/25+Aberdeen+St,+Perth+WA+6000/@-31.9473017,115.8587275,17z/data=!3m1!4b1!4m6!3m5!1s0x2a32bad0ff8118e1:0xc7cd20e43cdd3ed8!8m2!3d-31.9473063!4d115.8613024!16s%2Fg%2F12hxml0_c?entry=ttu&g_ep=EgoyMDI2MDQyNy4wIKXMDSoASAFQAw%3D%3D" target="_blank">25 Aberdeen St, Perth WA 6000</a></li>
             </ul>
         </section>
     </div>
