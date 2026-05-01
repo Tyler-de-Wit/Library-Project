e9d96f7645dec7d16868c6d42895a896f08dba72
Tyler <87571592+Tila05@users.noreply.github.com>
2026-05-01 19:29:59 +0800

    Update the sizing for the default post thumbnail and change the sizing for the image in the article cards to fit their aspect ratio

--- themes/libraryproject/template-parts/content-archive.php
+++ themes/libraryproject/template-parts/content-archive.php
@@ -4,9 +4,9 @@
     <div class="card-header">
 
         <?php if ( has_post_thumbnail() ): ?>
-            <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" class="card-img-top mb-2 mt-2" alt="Blog Post Thumbnail" height="250" width="auto">
+            <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" class="card-img-top mb-2 mt-2" alt="Blog Post Thumbnail" height="300" width="auto">
         <?php else: ?>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/default-post-thumbnail.jpg'; ?>" class="card-img-top mb-2 mt-2" alt="Blog Post Thumbnail" height="250" width="auto">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/default-post-thumbnail.jpg'; ?>" class="card-img-top mb-2 mt-2" alt="Blog Post Thumbnail" height="300" width="auto">
         <?php endif; ?>
 
         <a href="<?php the_permalink(); ?>" class="text-reset text-decoration-none">
